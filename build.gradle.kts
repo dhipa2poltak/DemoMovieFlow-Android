1d92becdfb6f76661b1b371d56538ba44e3d8ac5
Dhipa Poltak <dhipa2poltak@gmail.com>
2024-02-19 07:19:57 +0700

    integrating JaCoCo

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,47 @@ plugins {
   id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
   id("com.android.library") version "8.1.0" apply false
   id("com.google.dagger.hilt.android") version "2.44" apply false
+  id("jacoco")
+}
+
+jacoco {
+  toolVersion = "0.8.11"
+}
+
+apply(from = "jacoco/project.gradle.kts")
+
+subprojects {
+  afterEvaluate {
+    val projectName = this.name
+
+    if (projectName != "features") {
+      println("Applying JaCoCo for module-> $projectName")
+      this.apply(from = "${this.rootDir}/jacoco/module.gradle.kts")
+    }
+  }
+}
+
+tasks.create(name = "codeCoverModules") {
+  dependsOn(":domain:jacocoTestReport")
+  dependsOn(":data:jacocoTestReport")
+  dependsOn(":app:testProdDebugUnitTestCoverage")
+
+  subprojects.forEach { proj ->
+    val projectName = proj.name
+
+    if (projectName != "domain" && projectName != "data" && projectName != "app") {
+      if (projectName != "features") {
+        if (projectName == "framework" || projectName == "navigation") {
+          dependsOn(":${projectName}:testDebugUnitTestCoverage")
+        } else {
+          dependsOn(":features:${projectName}:testDebugUnitTestCoverage")
+        }
+      }
+    }
+  }
+}
+
+tasks.create<GradleBuild>(name = "allDebugCodeCoverage") {
+  tasks = listOf("allDebugCoverage")
+  mustRunAfter("codeCoverModules")
 }
